2f54e81972e9e7dc9d61df6211dc58c177183b63
Marcy <nadaso8th@gmail.com>
2023-03-02 22:48:30 -0600

    general refactoring work

--- MARlea_simulator/src/reaction_network/mod.rs
+++ MARlea_simulator/src/reaction_network/mod.rs
@@ -11,50 +11,53 @@ struct ReactionNetwork <'reacting> {
 
 impl<'reacting> ReactionNetwork <'reacting> {
 
-    /// Returns the subset of local reactions set is possible based on the Species.count values in solution
-    fn get_possible_reactions (&self) -> HashSet<Reaction<'reacting>> {
-        
-        let mut possible_reactions: HashSet<Reaction<'reacting>> = HashSet::new();
-
-            for reaction in &self.reactions {
-
-                let mut reaction_possible = true;
-
-                for reactant in &reaction.reactants {
-
-                    if reactant.coefficient as u64 > reactant.species.count {
-                        reaction_possible = false;
-                        break;
-                    }
-
-                }
-
-                if reaction_possible {
-                    if possible_reactions.insert(reaction.clone()) { }
-                }
-            }
-        return possible_reactions
-    }
-
     /// Randomly selects a reaction from the provided set using a probablility generated from the total number of reactions with reach one scaled by reaction rate,
-    fn get_next_reaction(possible_reactions: HashSet<Reaction<'reacting>>) -> &'reacting Reaction<'reacting> {
-        let mut next_reaction: &Reaction = todo!();
-    
+    pub fn get_next_reaction(possible_reactions: HashSet<Reaction<'reacting>>) -> &'reacting Reaction<'reacting> {
+        
+        index = rand::random()
         
         return next_reaction;
     }
 
     /// Updates local solution with the effects of a given reaction
-    fn react(& mut self, reaction: &'reacting Reaction<'reacting>) {
+    pub fn react(& mut self, reaction: &'reacting Reaction<'reacting>) {
         
-        for reactant in &reaction.reactants {
-            self.solution.entry(reactant.species)
-            .and_modify(|species| species.count -= u64::from(reactant.coefficient));
+        // this could maybe be done by using dedicated functions in Term struct which would allow for storage of species in a hash set
+        for reactant in reaction.get_reactants() {
+            self.solution.entry(reactant.get_species())
+            .and_modify(|species| species.set_count(species.get_count() - reactant.get_coefficient()));
         }
 
-        for product in &reaction.products {
-            self.solution.entry(product.species)
-            .and_modify(|species| species.count -= u64::from(product.coefficient));
+        for product in reaction.get_products() {
+            self.solution.entry(product.get_species())
+            .and_modify(|species| species.set_count(species.get_count() + product.get_coefficient()));
         }
+
+        return;
     }
+
+    /// Returns the subset of local reactions set is possible based on the Species.count values in solution
+    fn get_possible_reactions (&self) -> HashSet<Reaction<'reacting>> {
+    
+        let mut possible_reactions: HashSet<Reaction<'reacting>> = HashSet::new();
+
+            for reaction in &self.reactions {
+                if reaction.is_possible() {
+                    if possible_reactions.insert(reaction.clone()) { }
+                }
+            }
+
+        return possible_reactions;
+    }
+
+    /// takes outputs the total sum of the reaction rates in the provided hash set of reactions
+    fn sum_reaction_rates (reactions: HashSet<Reaction<'reacting>>) -> u64 {
+        let mut sum: u64 = 0; 
+        for reaction in reactions {
+            sum += reaction.get_reaction_rate();
+        }
+        return sum;
+    }
+
+
 }
